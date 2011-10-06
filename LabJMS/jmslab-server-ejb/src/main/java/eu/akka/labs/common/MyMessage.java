d699d2190eabc3f45fc73733ca07fae6b29687ba
Laurent BARESSE <l.baresse@akka.eu>
2011-10-06 17:10:17 +0200

    Add LabJMS v.1.0

---
+++ LabJMS/jmslab-server-ejb/src/main/java/eu/akka/labs/common/MyMessage.java
@@ -0,0 +1,24 @@
+package eu.akka.labs.common;
+
+import java.io.Serializable;
+
+public class MyMessage implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	public String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "MyMessage [message=" + message + "]";
+	}
+	
+}
