c435adb6e4fb12106266457b31c79a339b370ba7
Laurent BARESSE <l.baresse@akka.eu>
2011-10-10 13:58:43 +0200

    Add AS startup singleton bean lab
    This bean illustrates how a startup bean can be developed for instance
    to start EJB Timers or perform resources initialization on server start
    up and how to release resources when the server is shut down.

---
+++ LabEJB31/lab_ejb31-server-ejb/src/main/java/eu/akka/labs/server/MyStartupBean.java
@@ -0,0 +1,40 @@
+package eu.akka.labs.server;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
+import org.apache.log4j.Logger;
+
+/**
+ * This Bean is used to initialize things at application server startup and free resources at application server shutdown. 
+ * By using the @Startup annotation, you can force the container to instantiate the singleton instance during 
+ * application initialization.
+ * 
+ * @author AKKA
+ */
+@Singleton
+@Startup
+@SuppressWarnings("unused")
+public class MyStartupBean{
+
+   Logger logger = Logger.getLogger(MyStartupBean.class);
+	
+
+   /**
+    * @PostConstruct method that is guaranteed to be called at startup time.
+    */
+   @PostConstruct
+   private void startup() { 
+	   logger.info("@PostConstruct startup : done");
+   }
+
+   /**
+    * @PreDestroy method for a singleton is guaranteed to be called when the application is shutting down.
+    */
+   @PreDestroy
+   private void shutdown() { 
+	   logger.info("@PreDestroy shutdown : done");
+   }
+}
